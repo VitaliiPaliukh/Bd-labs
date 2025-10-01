660222535d3b53ea35ee35a9a49962cbe31c6a64
VitaliiPaliukh <vitalikpaliukh@gmail.com>
2025-10-01 22:36:52 +0300

    change

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def before_request():
 
 @app.route('/')
 def home():
-    return "Welcome to the Airline Database API111111111!"
+    return "Welcome to the Airline Database APIshka!"
 
 app.register_blueprint(connected_flights_bp, url_prefix='/api')
 app.register_blueprint(user_bp, url_prefix='/api')
